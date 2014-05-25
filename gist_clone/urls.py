5bf221df9516d0fbd5ccd054301996926372caa0
Vladislavs Burakovs <solomonchild@gmail.com>
2014-05-25 20:46:06 +0300

    Static test

--- gist_clone/urls.py
+++ gist_clone/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gist_clone.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^', include('gist2.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+if settings.DEBUG:
+  urlpatterns += patterns('',
+       (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/path/to/media'})
+   )
